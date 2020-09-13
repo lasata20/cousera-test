8e49ccbe4eef002cf1f6436ec8c4ed1865ed887b
lasata20 <lasatastha20@gmail.com>
2020-09-13 14:26:34 +0545

    Assignment

---
+++ mod4_solution/script.js
@@ -0,0 +1,38 @@
+
+( function () {
+
+var names = ["Harry", "John", "Joe", "Leo", "Paul", "Jimmy", "Laura", "Anny", "Jacob", "Marie"];
+
+( function(window){
+var helloSpeaker= {};
+
+  var speakWord="Hello";
+  helloSpeaker.speak = function (names) {
+  console.log(speakWord + " " + names);
+  }
+  window.helloSpeaker=helloSpeaker;
+})(window);
+
+
+( function(window){
+var byeSpeaker={};
+
+var speakWord = "Good Bye";
+byeSpeaker.speak=function(names) {
+  console.log(speakWord + " " + names);
+}
+window.byeSpeaker=byeSpeaker;
+})(window);
+
+
+for (var i = 0; i < names.length; i++) {
+
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter == 'j' || firstLetter == 'J') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+})();
